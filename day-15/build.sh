1a0cfdb2a4a02b0e1dd4cb84a7f93cd314e103e6
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-10-06 11:07:46 +0530

    🇮🇳

--- day-15/build.sh
+++ day-15/build.sh
@@ -5,3 +5,24 @@ mvn clean package -DskipTests
 docker build -t customer-service:latest .
 cd ..
 
+cd ./employee-service
+mvn clean package -DskipTests
+docker build -t employee-service:latest .
+cd ..
+
+cd ./product-service
+mvn clean package -DskipTests
+docker build -t product-service:latest .
+cd ..
+
+
+cd ./gateway-service
+mvn clean package -DskipTests
+docker build -t gateway-service:latest .
+cd ..
+
+cd ./eureka-service
+mvn clean package -DskipTests
+docker build -t eureka-service:latest .
+cd ..
+
--- day-15/customer-service/src/main/java/co/vinod/training/CustomerServiceApplication.java
+++ day-15/customer-service/src/main/java/co/vinod/training/CustomerServiceApplication.java
@@ -2,12 +2,14 @@ package co.vinod.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableSwagger2 // starts building the api cods (JSON) by using the docket bean
 public class CustomerServiceApplication {
